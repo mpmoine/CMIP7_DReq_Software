9b0f9cc4f331952124d853df99b3d2ee4b2653b9
rigoudyg <45363600+rigoudyg@users.noreply.github.com>
2025-07-10 17:16:29 +0200

    Bugfix for time subset (#154)

--- data_request_api/data_request_api/content/dump_transformation.py
+++ data_request_api/data_request_api/content/dump_transformation.py
@@ -416,7 +416,7 @@ def split_content_one_base(content):
         "opportunities": [("experiment_groups", list, list()),
                           ("variable_groups", list, list()),
                           ("data_request_themes", list, list()),
-                          ("time_subsets", list, ["80ac3156-a698-11ef-914a-613c0433d878", ]),
+                          ("time_subsets", list, ["link::80ac3156-a698-11ef-914a-613c0433d878", ]),
                           ("mips", list, list())],
         "variable_groups": [("variables", list, list()),
                             ("mips", list, list()),
